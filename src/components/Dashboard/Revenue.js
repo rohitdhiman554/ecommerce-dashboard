8a5d0e10e1975f081ecd55f497c801b849b36593
Rohit Dhiman <rohitdhiman937@gmail.com>
2024-03-02 00:11:00 +0530

    few changes

--- src/components/Dashboard/Revenue.js
+++ src/components/Dashboard/Revenue.js
@@ -1,39 +1,47 @@
-import { AreaChart, Card } from "@tremor/react";
+import { BarChart, Card } from "@tremor/react";
 
 const chartdata = [
-    {
-        date: "Jan 23",
-        "Route Requests": 289,
-        "Station Requests": 233,
-    },
+    { "name": "January", "transactions": 305 },
+    { "name": "February", "transactions": 231 },
+    { "name": "March", "transactions": 267 },
+    { "name": "April", "transactions": 236 },
+    { "name": "May", "transactions": 338 },
+    { "name": "June", "transactions": 274 },
+    { "name": "July", "transactions": 91 },
+    { "name": "August", "transactions": 337 },
+    { "name": "September", "transactions": 27 },
+    { "name": "October", "transactions": 152 },
+    { "name": "November", "transactions": 296 },
+    { "name": "December", "transactions": 119 }
+]
 
-    {
-        date: "Oct 23",
-        "Route Requests": 283,
-        "Station Requests": 247,
-    },
-];
+
 
 export default function Example() {
+    const dataFormatter = (number) =>
+        Intl.NumberFormat('us').format(number).toString();
 
 
     return (
         <div className="w-full h-full">
             <Card className="max-w-4xl">
                 <span className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-                    Total Requests
+                    Total Transactions
                 </span>
                 <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                    6,568
+                    2,673
                 </p>
-                <AreaChart
-                    className="mt-2 h-80"
+                <BarChart
                     data={chartdata}
-                    index="date"
-                    categories={["Route Requests", "Station Requests"]}
-                    colors={["indigo", "rose"]}
-                    yAxisWidth={33}
+                    index="name"
+                    categories={['transactions']}
+                    colors={['blue']}
+                    valueFormatter={dataFormatter}
+                    yAxisWidth={80}
+                    onValueChange={(v) => console.log(v)}
                 />
+
+
 
 
             </Card>
