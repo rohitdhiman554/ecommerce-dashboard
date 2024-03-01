98dc5bf65f4e1d1c6c20137d1693b0f87408935c
Rohit Dhiman <rohitdhiman937@gmail.com>
2024-03-01 23:33:40 +0530

    ui changes

--- src/components/Dashboard/Stocks.js
+++ src/components/Dashboard/Stocks.js
@@ -4,84 +4,97 @@ import { useState } from "react";
 
 const datahero = [
     {
-        name: 'New York',
-        sales: 980,
+        "name": "New York",
+        "sales": 980,
+        "growthRate": "5%"
     },
     {
-        name: 'London',
-        sales: 456,
+        "name": "London",
+        "sales": 456,
+        "growthRate": "3%"
     },
     {
-        name: 'Hong Kong',
-        sales: 390,
+        "name": "Hong Kong",
+        "sales": 390,
+        "growthRate": "2%"
     },
     {
-        name: 'San Francisco',
-        sales: 240,
+        "name": "San Francisco",
+        "sales": 240,
+        "growthRate": "4%"
     },
     {
-        name: 'Singapore',
-        sales: 190,
-    },
+        "name": "Singapore",
+        "sales": 190,
+        "growthRate": "6%"
+    }
 ]
+
 
 const Stocks = () => {
 
     const [value, setValue] = useState(null);
-
-    console.log("calue", value)
 
     const dataFormatter = (number) =>
         `$ ${Intl.NumberFormat('us').format(number).toString()}`;
 
     return (
         <div className="w-full">
-            <Card className="max-w-4xl h-full border-2">
+            <Card className="max-w-4xl h-full border-2 flex flex-col">
                 <span className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-                    Total Requests
+                    Overall Sales
                 </span>
                 <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
                     6,568
                 </p>
-                <div className="flex items-center justify-center space-x-6">
-                    <DonutChart
-                        showTooltip
-                        showAnimation
-                        showLabel
-                        data={datahero}
-                        category="sales"
-                        index="name"
-                        valueFormatter={dataFormatter}
-                        colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
-                        className="w-40"
-                        onValueChange={(v) => !v && setValue(v)}
-                    />
+                <div className="flex flex-col gap-6 mt-6 ">
+                    <div className="flex items-center justify-center space-x-6">
+                        <DonutChart
+                            showTooltip
+                            showAnimation
+                            showLabel
+                            data={datahero}
+                            category="sales"
+                            index="name"
+                            valueFormatter={dataFormatter}
+                            colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
+                            className="w-40"
+                            onValueChange={(v) => setValue(v)}
+                        />
 
-                    <Legend
-                        categories={['New York', 'London', 'Hong Kong', 'San Francisco', 'Singapore']}
-                        colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
-                        className="max-w-xs"
-                    />
+                        <Legend
+                            categories={['New York', 'London', 'Hong Kong', 'San Francisco', 'Singapore']}
+                            colors={['blue', 'cyan', 'indigo', 'violet', 'fuchsia']}
+                            className="max-w-xs"
+                        />
+                    </div>
+                    <div className="w-full h-full mt-5">
+                        <div className="flex gap-5">
+                            <Card>
+                                <div className="flex flex-col">
+                                    <span className="text-lg font-semibold">City Name</span>
+                                    <span className="text-sm">{value ? value.name : '--'}</span>
+                                </div>
+                            </Card>
+                            <Card>
+                                <div className="flex flex-col">
+                                    <span className="text-lg font-semibold">Total Sales</span>
+                                    <span className="text-sm">{value ? value.sales : '--'}</span>
+                                </div>
+                            </Card>
+                            <Card>
+                                <div className="flex flex-col">
+                                    <span className="text-lg font-semibold">Growth Rate</span>
+                                    <span className="text-sm">{value ? value.growthRate : '--'}</span>
+                                </div>
+                            </Card>
+
+
+
+
+                        </div>
+                    </div>
                 </div>
-                {value ? <div className="w-full h-full mt-5">
-                    <Card className="w-full flex gap-10">
-                        <Card>
-                            <div className="flex flex-col">
-                                <span className="text-lg font-semibold">City Name</span>
-                                <span className="text-sm">{value.name}</span>
-                            </div>
-                        </Card>
-                        <Card>
-                            <div className="flex flex-col">
-                                <span className="text-lg font-semibold">Total Sales</span>
-                                <span className="text-sm">{value.sales}</span>
-                            </div>
-                        </Card>
-
-
-                    </Card>
-                </div> : null}
-
 
             </Card>
         </div>
