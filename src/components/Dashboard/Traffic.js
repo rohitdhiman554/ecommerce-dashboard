8c587e754971adfbe70ee024f720eec0b7e46074
Rohit Dhiman <rohitdhiman937@gmail.com>
2024-03-02 21:00:27 +0530

    moving const's to file

--- src/components/Dashboard/Traffic.js
+++ src/components/Dashboard/Traffic.js
@@ -1,25 +1,10 @@
 import React, { useState } from 'react';
 import { Card, LineChart, Text, Title } from '@tremor/react';
+import { TRAFFIC_DATA, TRAFFIC_DATA_CATEGORIES } from '../../utils/constants';
 
 
-
-const dataCategories = ['SearchPaid', 'Referrals', 'Direct', 'SemiAnalysis'];
-const chartdata = [
-    { "date": "Jan 23", "SearchPaid": 73.76, "Referrals": 28.67, "Direct": 2.7, "SemiAnalysis": 11.23 },
-    { "date": "Feb 23", "SearchPaid": 62.48, "Referrals": 48.7, "Direct": 24.68, "SemiAnalysis": 59.22 },
-    { "date": "Mar 23", "SearchPaid": 10.89, "Referrals": 23.02, "Direct": 80.55, "SemiAnalysis": 34.03 },
-    { "date": "Apr 23", "SearchPaid": 46.54, "Referrals": 45.94, "Direct": 23.84, "SemiAnalysis": 68.58 },
-    { "date": "May 23", "SearchPaid": 88.03, "Referrals": 39.74, "Direct": 75.1, "SemiAnalysis": 95.06 },
-    { "date": "Jun 23", "SearchPaid": 48.12, "Referrals": 72.89, "Direct": 67.07, "SemiAnalysis": 91.67 },
-    { "date": "Jul 23", "SearchPaid": 55.67, "Referrals": 56.28, "Direct": 31.74, "SemiAnalysis": 20.16 },
-    { "date": "Aug 23", "SearchPaid": 53.1, "Referrals": 41.36, "Direct": 90.39, "SemiAnalysis": 81.63 },
-    { "date": "Sep 23", "SearchPaid": 18.39, "Referrals": 41.49, "Direct": 15.08, "SemiAnalysis": 46.62 },
-    { "date": "Oct 23", "SearchPaid": 99.35, "Referrals": 97.48, "Direct": 18.96, "SemiAnalysis": 60.85 },
-    { "date": "Nov 23", "SearchPaid": 82.55, "Referrals": 2.16, "Direct": 31.39, "SemiAnalysis": 48.31 },
-    { "date": "Dec 23", "SearchPaid": 90.89, "Referrals": 60.71, "Direct": 79.52, "SemiAnalysis": 78.99 }
-]
 export function Traffic() {
-    const [activeCategories, setActiveCategories] = useState(dataCategories);
+    const [activeCategories, setActiveCategories] = useState(TRAFFIC_DATA_CATEGORIES);
 
     const toggleCategory = (category) => {
         if (activeCategories.includes(category)) {
@@ -32,7 +17,7 @@ export function Traffic() {
     const dataFormatter = (number) => `$${Intl.NumberFormat('us').format(number).toString()}`;
 
 
-    const filteredChartData = chartdata.map(entry => {
+    const filteredChartData = TRAFFIC_DATA.map(entry => {
         const filteredEntry = { date: entry.date };
         activeCategories.forEach(category => {
             if (entry[category] !== undefined) {
@@ -47,7 +32,7 @@ export function Traffic() {
             <Title>Ecommerce Industry Traffic Trend-2023</Title>
             <Text>From Jan 2023 - Dec 2023</Text>
             <div className="flex flex-wrap my-4">
-                {dataCategories.map(category => (
+                {TRAFFIC_DATA_CATEGORIES.map(category => (
                     <div key={category} className="flex items-center mr-4 mb-2">
                         <input
                             id={`checkbox-${category}`}
